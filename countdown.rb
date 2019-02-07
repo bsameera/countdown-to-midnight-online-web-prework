1102f076af94d7be368fea6ae83d43007b316f22
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 02:37:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = number
+  phrase = "HAPPY NEW YEAR!"
+  while counter > 0  
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  phrase
+end
+
+def countdown_with_sleep(num)
+  counter = num
+  while counter > 0   
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -= 1
+  end
 end
