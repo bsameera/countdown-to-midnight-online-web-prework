8e1cd672499e193f2f829d4f41f77f5c817502f3
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 02:32:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,5 +11,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(num)
-  
+  counter = number
+  while counter > -1  
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -= 1
+  end
 end
