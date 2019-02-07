e0d0fe72ec7adbc3af3f262fec4cef7dd375171c
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 02:34:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   counter = number
   phrase = "HAPPY NEW YEAR!"
-  while counter > -1  
+  while counter > 0  
     puts "#{counter} SECOND(S)!"
     counter -= 1
   end
@@ -12,7 +12,7 @@ end
 
 def countdown_with_sleep(num)
   counter = number
-  while counter > -1  
+  while counter > 0   
     puts "#{counter} SECOND(S)!"
     sleep(1)
     counter -= 1
